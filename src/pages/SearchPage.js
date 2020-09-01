7fc23f21a508b0e5c84d4c7355bf5bbe625883f6
sailesh-mahat <sailesharsenal@gmail.com>
2020-09-01 09:01:59 +0200

    app complete

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -70,9 +70,35 @@ function SearchPage() {
                     </div>
                 </div>
             </div>
-            <div className='searchPage__results'>
+
+            {true && (
+                <div className='searchPage__results'>
+                    <p className='searchPage__resultCount'>
+                        About {data?.searchInformation.formattedTotalResults} results
+                        ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+                    </p>
+
+                    {data?.items.map(item => (
+                        <div className='searchPage__result'>
+                            <a className='searchPage__resultLink' href={item.link}>
+                                {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
+                                    <img className='searchPage__resultImage' src={item.pagemap?.cse_image[0]?.src} alt="" />
+                                )}
+
+                                {item.displayLink}
+                            </a>
+                            <a className='searchPage__resultTitle' href={item.link}>
+                                <h2>{item.title}</h2>
+                            </a>
+                            <p className='searchPage__resultSnippet'>
+                                {item.snippet}
+                            </p>
+                        </div>
+                    ))}
                 
-            </div>            
+                </div> 
+            )}  
+                       
         </div>
     )
 }
